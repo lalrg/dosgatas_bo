4b5ad4be82c603a1f29a9b44383d253917042d9d
Luis Richmond <lalrg@outlook.com>
2025-02-23 07:08:40 -0600

    create product FE ready

--- dosgatas_recipes/src/sections/products/create/index.tsx
+++ dosgatas_recipes/src/sections/products/create/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type { FormProps } from 'antd';
-import { Button, InputNumber, Form, Input, Row, Col } from 'antd';
+import { Button, InputNumber, Form, Input, Row, Col, message } from 'antd';
+import { useNavigationDispatch } from '../../../shared/context/NavigationContext';
 
 type FieldType = {
   name?: string;
@@ -8,54 +9,80 @@ type FieldType = {
   cost?: number;
 };
 
-const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-  console.log('Success:', values);
+const CreateProduct: React.FC = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const navigationDispatch = useNavigationDispatch();
+
+  useEffect(() => {
+    if (isSubmitted) {
+      if (!isSuccess) {
+        messageApi.success('Producto creado correctamente');
+        setTimeout(() => {
+          navigationDispatch({ route: 'listProduct' });
+        }, 1000);
+      } else {
+        messageApi.error('Error al crear el producto');
+      }
+      setIsSubmitted(false);
+    }
+  }, [isSuccess, isSubmitted, messageApi, navigationDispatch]);
+
+  const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    // here goes the backend logic
+    setIsSuccess(true);
+    setIsSubmitted(true);
+  };
+
+  const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+    setIsSuccess(false);
+    setIsSubmitted(true);
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Form
+        name="createProduct"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Row align={"middle"} justify={"center"}>
+          <Col>
+            <Form.Item<FieldType>
+              label="Nombre"
+              name="name"
+              rules={[{ required: true, message: 'Nombre del producto!' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item<FieldType>
+              label="Descripcion"
+              name="description"
+              rules={[{ required: true, message: 'Descripcion del producto!' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item<FieldType>
+              label="Costo"
+              name="cost"
+              rules={[{ required: true, message: 'Costo del producto!' }]}
+            >
+              <InputNumber />
+            </Form.Item>
+
+            <Form.Item label={null}>
+              <Button type="primary" htmlType="submit">
+                Crear
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  )
 };
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
-};
-
-const CreateProduct: React.FC = () => (
-  <Form
-    name="createProduct"
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Row align={"middle"} justify={"center"}>
-      <Col>
-        <Form.Item<FieldType>
-          label="Nombre"
-          name="name"
-          rules={[{ required: true, message: 'Nombre del producto!' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Descripcion"
-          name="description"
-          rules={[{ required: true, message: 'Descripcion del producto!' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Costo"
-          name="cost"
-          rules={[{ required: true, message: 'Costo del producto!' }]}
-        >
-          <InputNumber />
-        </Form.Item>
-
-
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit">
-            Crear
-          </Button>
-        </Form.Item>
-      </Col>
-    </Row>
-  </Form>
-);
 
 export default CreateProduct;
